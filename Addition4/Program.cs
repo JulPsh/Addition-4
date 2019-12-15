b0bb1bd906a6458e6dd8c14bcef45045bb7e604a
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-12-15 22:51:56 +0300

    Addition-4

---
+++ Addition4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Addition4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            int y = 15;
+            int z = x + y;
+            Console.WriteLine($"Первое число:{x}");
+            Console.WriteLine($"Второе число:{y}");
+            Console.WriteLine($"Сумма:{x}+{y}={z}");
+        }
+    }
+}
